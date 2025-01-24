fd0b0b0018bd82b815499202201d2ed0890b91b1
root <root@localhost>
2025-01-24 21:32:30 +0100

    auto publish 1737750750799

--- src/main.cpp
+++ src/main.cpp
@@ -2,44 +2,8 @@
 using namespace std;
 
 int main() {
-    char grade ='A';
-    cout << grade;
-    
-    return 0;
-}
-#include <iostream>
-using namespace std;
-
-int main() {
-    char grade ='A';
+    char grade = 'A';
     cout << "Grade is: "<< grade;
     
     return 0;
 }
-  
-   
-    
- 
-
-
-    
-    
-    
-    
-
-
-
-  
-    
-      
-  
-
-
-
-
-
-    
-   
-    
-    
-
